7eb5ad476dcaf7ff3127bf8cd6d5981ea5211f3f
root <root@DESKTOP-51N377A.localdomain>
2022-01-21 19:54:55 -0300

    layout responsivo

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,16 +1,36 @@
 import style from './styles.module.scss'
+import {AiOutlineMenu} from 'react-icons/ai'
+import { useState } from 'react'
 export function Header(){
+  const [menu,setMenu] = useState(false)
+  function handleMenu(){
+    setMenu(!menu)
+  }
   return(
+    <>
   <header className={style.customHeader}>
     <h1> BELILO</h1> 
-    <nav>
+    <nav className={style.nav}>
       <a href="#">Inicio</a>
       <a href="#">Blog</a>
       <a href="#">Portifolio</a>
       <a href="#">Tecnologias</a>
       <a href="#">Serviços</a>
     </nav>
-      <button> Contact</button>
+    <AiOutlineMenu  className={style.showMenu} size={30} onClick={handleMenu}/>
+
+    
+    
+{/*       <button> Contact</button> */}
   </header>
+      {menu && <nav className={style.navMobile}>
+      <a href="#">Inicio</a>
+      <a href="#">Blog</a>
+      <a href="#">Portifolio</a>
+      <a href="#">Tecnologias</a>
+      <a href="#">Serviços</a>
+    </nav>}
+    
+  </>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,7 @@ import '../styles/globals.scss'
 function MyApp({ Component, pageProps }:AppProps) {
   return (
     <>
-    <Head>
-      <meta name="viewport" content="viewport-fit=cover" />
-    </Head>
+
     <Component {...pageProps} />
   </>
   )

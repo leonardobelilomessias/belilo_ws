7251e2ce9e5eab914e5e933d49ff4cfc20d6ce2b
leonardobmessias <71788533+leonardobmessias@users.noreply.github.com>
2022-04-29 15:52:07 -0300

    Reestilização da inicial

--- src/Components/Main/Cases/index.tsx
+++ src/Components/Main/Cases/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import style from './cases.style.module.scss'
+import Natcults from '../../../../public/natcults.png'
+import Target from '../../../../public/target.png'
+import FastFree from '../../../../public/fastfreee.png'
+
+import Image from 'next/image'
 function Cases() {
   return (
     <>
@@ -7,15 +12,43 @@ function Cases() {
     <section className={style.examples}>
           <h1>Casos de Sucesso</h1>
           <div className={style.models}>
-            <div className={style.model}><p>Natcults</p></div>
-            <div className={style.model}><p>Puzzle Piece</p></div>
-            <div className={style.model}><p>Amazon Delivery</p></div>
+
+            <div className={style.model}>
+              <div className={style.background}></div>
+              <div className={style.imgBackground}><Image src={Natcults} /> </div>
+
+            </div>
+
+            <div className={style.model}>
+              <div className={style.background}></div>
+              <div className={style.imgBackground} ><Image   src={Target} /> </div>
+            </div>
+            <div className={style.model}>
+              <div className={style.background}></div>
+              <div className={style.imgBackground} >  <Image src={FastFree} /> </div>
+             
+
+            </div>
           </div>
           <div className={style.models}>
-            <div className={style.model}><p>HubStack</p></div>
-            <div className={style.model}><p>Legacy Conect</p></div>
-            <div className={style.model}><p>GoLana</p></div>
-          </div>
+
+<div className={style.model}>
+  <div className={style.background}></div>
+  <div className={style.imgBackground}><Image src={Natcults} /> </div>
+
+</div>
+
+<div className={style.model}>
+  <div className={style.background}></div>
+  <div className={style.imgBackground} ><Image   src={Target} /> </div>
+</div>
+<div className={style.model}>
+  <div className={style.background}></div>
+  <div className={style.imgBackground} >  <Image src={FastFree} /> </div>
+ 
+
+</div>
+</div>
         </section></>
   )
 }
--- src/Components/Main/Schedule/index.tsx
+++ src/Components/Main/Schedule/index.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import style from './schedule.style.module.scss'
 
 const schedule= [
-  {number:1,title:"Envie sua ideia", description:"Você nos envia os detalhes da sua ideia ou negocio"},
-  {number:2,title:"Modelagem e feedback", description:"Você nos envia os detalhes da sua ideia ou negocio"},
-  {number:3,title:"Implementação", description:"Você nos envia os detalhes da sua ideia ou negocio"}
+  {number:1,title:"Envio", description:"Você nos envia os detalhes  de como você gostaria que fosse disponibilidada  sua ideia ou negocio"},
+  {number:2,title:"Modelagem", description:"Prototipamos um produto com todos os detalhes enviado para lhe apresentar"},
+  {number:3,title:"Implementação", description:"Entrega do projeto finalem uma hospedagem em nuvem, dominio pesonalizado disponivel para toda web ou lojas de aplicativos"}
 ]
 function Schedule() {
   return (
     <div className={style.container}>
-      <h1>Tudo feito em três simples passos <br/> sem complicação.</h1>
+      <h1>Tudo em três simples passos <br/> sem complicação.</h1>
       <div className={style.box}>
         {schedule.map(step=>(
           <>

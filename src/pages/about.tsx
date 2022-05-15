a7afc5ba97997497dd0a6fd133a9257bae7986ac
leonardobmessias <71788533+leonardobmessias@users.noreply.github.com>
2022-05-15 19:17:14 -0300

    Organização do projeto

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import Result from '../../public/result.jpg'
-import Develop from '../../public/develop.jpg'
-import Idea from '../../public/idea.jpg'
-import style from '../styles/about.style.module.scss' 
-import Head from 'next/head'
+import React from 'react';
+import Result from '../../public/result.jpg';
+import Develop from '../../public/develop.jpg';
+import Idea from '../../public/idea.jpg';
+import style from '../styles/about.style.module.scss';
+import Head from 'next/head';
 import Image from 'next/image';
+import Team from '../../public/banner.jpg';
+import { bigTechs } from './bigTechs';
 const infoBox = [
   {title:'Quem somos ?',subTitle:'Somos Experts em Executar Ideias',description:'Fazemos produtos, ideias, sonhos e negocios sair do campo da imaginação e colocamos em pratica no mundo digital.'
   ,img:Result,cta:'Ver Serviços'},
   {title:'Oque Fazemos?',subTitle:'Potencializamos Resultados',description:' Autentou em 67,8% o numero de negocios feito na internet ou via aplicativos.Com as tecnologias mais avançadas damos poderes digitais para você atingir o seu potencial maximo,Alcançando novas pessoas, conquisando novos cliente e aumentando sua lucratividade ',img:Idea,cta:'Veja Casos de sucesso '},
   {title:'Como fazemos',subTitle:'Desenvolvemos Produtos tecnologicos',description:'Desenvolvemos serviços tecnológicos como sites institucionais, blogs, ecommercer e aplicativos e outros diversos tipos de ferramentas que auxiliam negócios físicos e digitais .',img:Develop,cta:`Fazer Simulação      `}
 ];
+
+
 
 function About() {
   return (
@@ -21,11 +25,10 @@ function About() {
     <main className={style.main}>
       <div className={style.head}>
       <div className={style.title}>
-        <h1>Bem vindo a democratização da tecnologia</h1>
+        <h1>A democratização da tecnologia</h1>
         <p>
           Nossa missão é fornecer produtos da mais alta qualidade de forma acessivel para o maior numero de pessoas.
-          Portanto desenvolvemos ferramentas poderosas porem de simples manuseio, facilitando com que o usuario final tenha 
-          plena capacidade de gerenciar seus planos no mundo digital
+          Portanto desenvolvemos ferramentas poderosas porem de simples manuseio.
         </p>
         </div>
       </div>
@@ -49,9 +52,28 @@ function About() {
             </div>
             </>  
           ))}
-    
-
-    </div>
+        </div>
+        <div className={style.secondSection}>
+          <div className={style.imageSecondSection}>
+          <Image alt='result'src={Team} layout='fill' objectFit='cover' />oi
+          </div>
+          <div className={style.textSecondSection}>
+            <h1> Ferramentas de ultima geração</h1>
+            <p>Crie site inovadores com alta performace com as tecnologias de ponta do mercado e saia na frente no desenvolvimento de suas ideas e negocios !</p>
+            <button>clique aqui e saiba</button>
+          </div>
+        </div>
+        <div className={style.felows}>
+          <h1>Parceria com as maiores Bigtechs do mercado</h1>
+          <div className={style.company}>
+              {bigTechs.map((company,key) => (
+                <div key={company.name} >
+                  <Image alt={company.name} src={company.img} layout='fill' objectFit='contain'/>
+                  
+                  </div>
+              ))}
+            </div>
+        </div>
 
   </main>
     </>
---
+++ src/pages/bigTechs.ts
@@ -0,0 +1,28 @@
+import Aws from '../../public/img/brands/aws.png'
+import Face from '../../public/img/brands/face.png'
+import Google from '../../public/img/brands/goole.png'
+import Mac from '../../public/img/brands/mac.png'
+import Micro from '../../public/img/brands/micro.png'
+
+export const bigTechs = [
+  {
+    name: 'Google',
+    img: Google
+  },
+  {
+    name: 'Facebook',
+    img: Face
+  },
+  {
+    name: 'Mac',
+    img: Mac
+  },
+  {
+    name: 'Amazon',
+    img: Aws
+  },
+  {
+    name: 'Microsoft',
+    img: Micro
+  },
+]

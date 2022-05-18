2a62932e629ce2a54f1a3c4597463e481079497c
leonardobmessias <71788533+leonardobmessias@users.noreply.github.com>
2022-05-18 17:28:01 -0300

    Edição cabeçalho

--- src/Components/Main/FirstSection/index.tsx
+++ src/Components/Main/FirstSection/index.tsx
@@ -1,6 +1,8 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import style from './firstSection.style.module.scss'
+import Cover from '../../../../public/cover2.jpg'
 
 function FirstSection() {
   return (
@@ -9,7 +11,7 @@ function FirstSection() {
         <div className={style.headLine}>
           <div className={style.textHeadline}>
             <h4>Desenvolvimento de sites e aplicativos</h4>
-            <h1>monstre valor <br/>Garanta respeito </h1>
+            <h1>monstre valor <br/>Garanta respeito</h1>
             <h3>Somos especialistas em tecnologia e damos poderes para alavancar suas ideias no mundo digital.</h3>
             <Link href="/simulator" >
               <a className={style.buttonHeadline} >
@@ -19,7 +21,7 @@ function FirstSection() {
           </div>
         </div>
         <div className={style.imageHeadline}>
-
+            <Image alt='woman power' src={Cover} layout='fill' objectFit='cover'/>
         </div>
       </div>
    </>

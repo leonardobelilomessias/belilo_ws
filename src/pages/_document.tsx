5019535838b74b0dd986bc0c8a50a220ebe3b202
leonardobmessias <71788533+leonardobmessias@users.noreply.github.com>
2022-05-21 12:54:00 -0300

    Correção google fontes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,8 @@ export default  class Mydocument extends Document{
     return(
       <Html >
         <Head>
-
+            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300&display=swap" rel="stylesheet"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com"  />
         
         </Head>
         <body>
